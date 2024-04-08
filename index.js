af34d1953b009d91f3d53777d1698e5c9063b560
AnasBInRiaz123 <saimshehzad2030@gmail.com>
2024-04-08 16:19:21 +0500

    final

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config();
-const {PORT}=require('./src/config/config')
-const routerUser = require('./src/routes/user.routes')
-const connectDb = require('./src/db/db');
-const emailRoutes = require('./src/routes/email.routes');
-const tokenRoutes = require('./src/routes/token.routes');
-const studentRoutes = require('./src/routes/student.routes')
-const jobRoutes = require('./src/routes/job.routes')
-const applicationRoutes = require('./src/routes/application.routes')
-const hiringRoutes = require('./src/routes/hiring.routes')
-const companyRoutes = require('./src/routes/company.routes')
-const app = express()
-app.use(cors())
-app.use(express.json())
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { PORT } = require("./src/config/config");
+const routerUser = require("./src/routes/user.routes");
+const connectDb = require("./src/db/db");
+const emailRoutes = require("./src/routes/email.routes");
+const tokenRoutes = require("./src/routes/token.routes");
+const studentRoutes = require("./src/routes/student.routes");
+const jobRoutes = require("./src/routes/job.routes");
+const applicationRoutes = require("./src/routes/application.routes");
+const hiringRoutes = require("./src/routes/hiring.routes");
+const companyRoutes = require("./src/routes/company.routes");
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-connectDb()
+connectDb();
 //routes
-app.use('/api',routerUser)
-app.use('/api',companyRoutes)
-app.use('/api',hiringRoutes)
-app.use('/api',applicationRoutes)
-app.use('/api',studentRoutes)
-app.use('/api',jobRoutes)
-app.use('/api',emailRoutes)
-app.use('/api',tokenRoutes)
-app.get("/",async (req, res) => {
-  res.json('Hello World');
+app.use("/api", routerUser);
+app.use("/api", companyRoutes);
+app.use("/api", hiringRoutes);
+app.use("/api", applicationRoutes);
+app.use("/api", studentRoutes);
+app.use("/api", jobRoutes);
+app.use("/api", emailRoutes);
+app.use("/api", tokenRoutes);
+app.get("/", async (req, res) => {
+  res.json("Hello World");
 });
 
-
 app.listen(PORT, () => console.log(`Server runing at PORT ${PORT}`));
-
-
 
 // const express = require('express')
 // const cors = require('cors')
@@ -79,4 +76,3 @@ app.listen(PORT, () => console.log(`Server runing at PORT ${PORT}`));
 // });
 
 // })
-
--- src/controller/company.controller.js
+++ src/controller/company.controller.js
@@ -1,55 +1,49 @@
-const User = require('../model/user.model')
-const Job = require('../model/job.model')
+const User = require("../model/user.model");
+const Job = require("../model/job.model");
 const deleteCompany = async (req, res) => {
+  //function to add two numbers
 
-    //function to add two numbers
-    
-    try {
-        const user = req?.user?.user?.role;
-        const { id,email } = req.query
-        if (user  === 'admin') {
-
-            if (!id || !email) {
-                res.status(401).send('Enter Email')
-            }
-            else {
-                const findUser = await User.findOne({ _id:id });
-                if (!findUser) {
-                    res.status(401).send('No Company Found')
-                    return
-                }
-              
-                    await User.deleteOne({ _id:id })
-                    await Job.deleteMany({ companyemail: email })
-                    res.status(200).json({ message: `${findUser.email} Deleted Successfully` })
-
-                
-            }
+  try {
+    const user = req?.user?.user?.role;
+    const { id, email } = req.query;
+    if (user === "admin") {
+      if (!id || !email) {
+        res.status(401).send("Enter Email");
+      } else {
+        const findUser = await User.findOne({ _id: id });
+        if (!findUser) {
+          res.status(401).send("No Company Found");
+          return;
         }
-        else {
 
-            res.status(520).json({ message: "You are not authorized" });
-        }
+        await User.deleteOne({ _id: id });
+        await Job.deleteMany({ companyemail: email });
+        res
+          .status(200)
+          .json({ message: `${findUser.email} Deleted Successfully` });
+      }
+    } else {
+      res.status(520).json({ message: "You are not authorized" });
     }
-    catch (error) {
-        return res.status(520).json({ message: "internal server error", error: error.message });
-    }
-}
-
+  } catch (error) {
+    return res
+      .status(520)
+      .json({ message: "internal server error", error: error.message });
+  }
+};
 
 const allCompanies = async (req, res) => {
-    try {
-        if (req?.user?.user?.role == 'admin') {
-            const users = await User.find({ role: 'company' });
-            res.status(200).json({ data: users, message: 'Commpanies Fetched' })
-        }
-        else {
-            res.status(401).send("you are not authorized")
-        }
+  try {
+    if (req?.user?.user?.role == "admin") {
+      const users = await User.find({ role: "company" });
+      res.status(200).json({ data: users, message: "Commpanies Fetched" });
+    } else {
+      res.status(401).send("you are not authorized");
+      // throw new Error({message:'You are not authorized'});
     }
-    catch (error) {
-        return res.status(520).json({ message: error.message });
-    }
-}
+  } catch (error) {
+    return res.status(520).json({ message: error.message });
+  }
+};
 
-module.exports = { deleteCompany, allCompanies }
+module.exports = { deleteCompany, allCompanies };
